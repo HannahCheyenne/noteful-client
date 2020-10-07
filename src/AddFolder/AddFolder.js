47a0a0652d15c6d7cd493b70eec90258cd202fc4
Hannah <HannahCheyenne@gmail.com>
2020-10-07 13:47:36 -0500

    added validation, fixed refresh

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -12,18 +12,45 @@ export default class AddFolder extends Component {
   }
   static contextType = ApiContext;
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: {
+        value: '',
+        touched: false
+      }
+    }
+  }
+
+  updateName(name) {
+    this.setState({
+      name: {
+        value: name,
+        touched: true
+      }
+    })
+  }
+
+  validateName(name) {
+    const { value, touched } = this.state.name
+    const { folders } = this.context
+
+    return (
+      typeof value === 'string' && value.length > 0 && touched && !folders.find(
+        folder => folder.name && folder.name.toLowerCase === value.toLowerCase()
+      )
+    )
+  }
+
   handleSubmit = e => {
     e.preventDefault()
-    const folder = {
-      name: e.target['folder-name'].value
-    }
-    fetch(`${config.API_ENDPOINT}/folders`, {
+    const newFolder = JSON.stringify({ name: this.state.name.value })
+    const options = {
       method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(folder),
-    })
+      headers: { 'content-type': 'application/json' },
+      body: newFolder,
+    }
+    fetch(`${config.API_ENDPOINT}/folders`, options)
       .then(res => {
         if (!res.ok)
           return res.json().then(e => Promise.reject(e))
@@ -31,7 +58,7 @@ export default class AddFolder extends Component {
       })
       .then(folder => {
         this.context.addFolder(folder)
-        this.props.history.push(`/folder/${folder.id}`)
+        this.props.history.push(`/`)
       })
       .catch(error => {
         console.error({ error })
@@ -47,10 +74,16 @@ export default class AddFolder extends Component {
             <label htmlFor='folder-name-input'>
               Name
             </label>
-            <input type='text' id='folder-name-input' name='folder-name' />
+            <input 
+              type='text' 
+              id='folder-name-input' 
+              name='folder-name' 
+              onChange={(e) => this.updateName(e.target.value)} />
           </div>
           <div className='buttons'>
-            <button type='submit'>
+            <button 
+              disabled={!this.validateName()}
+              type='submit'>
               Add folder
             </button>
           </div>
--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import NotefulForm from '../NotefulForm/NotefulForm'
 import ApiContext from '../ApiContext'
 import config from '../config'
+import CircleButton from '../CircleButton/CircleButton'
 import './AddNote.css'
 
 export default class AddNote extends Component {
@@ -12,28 +15,83 @@ export default class AddNote extends Component {
   }
   static contextType = ApiContext;
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: { value: '', touched: false },
+      content: { value: '', touched: false },
+      folder: { value: '' }
+    }
+  }
+
+  updateName = (name) => {
+    this.setState({
+      name: {
+        value: name,
+        touched: true
+      }
+    })
+  }
+
+  updateContent = (content) => {
+    this.setState({
+      content: {
+        value: content,
+        touched: true
+      }
+    })
+  }
+
+  updateFolder = (id) => {
+    this.setState({ 
+      folder: { 
+        value: id, 
+        touched: true 
+      } 
+    });
+  };
+
+  renderFolderSelection = () => {
+    return this.context.folders.map((folder) => {
+      return (
+        <option key={folder.id} value={folder.id}>
+          {folder.name}
+        </option>
+      )
+    })
+  }
+
+  validateName() {
+    const { value, touched } = this.state.name
+    return typeof value === 'string' && value.length > 0 && touched
+  }
+
+  validateFolder = () => {
+    return this.state.folder.value
+  }
+
   handleSubmit = e => {
     e.preventDefault()
-    const newNote = {
-      name: e.target['note-name'].value,
-      content: e.target['note-content'].value,
-      folder_id: e.target['note-folder-id'].value,
+    const { name, content, folder } = this.state
+    const newNote = JSON.stringify({
+      name: name.value,
+      content: content.value,
+      folder_id: folder.value,
       modified: new Date(),
-    }
-    console.log(newNote)
-    fetch(`${config.API_ENDPOINT}/notes`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(newNote),
     })
+    const options = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: newNote,
+    }
+    fetch(`${config.API_ENDPOINT}/notes`, options)
       .then(res => {
         if (!res.ok)
           return res.json().then(e => Promise.reject(e))
         return res.json()
       })
       .then(note => {
+        note.id = note.id.toString()
         this.context.addNote(note)
         this.props.history.push(`/`)
       })
@@ -42,8 +100,26 @@ export default class AddNote extends Component {
       })
   }
 
-  render() {
-    const { folders=[] } = this.context
+  renderAlt = () => {
+    return (
+      <>
+        <h2>Please create a folder first</h2>
+        <CircleButton
+          tag={Link}
+          to="/add-folder"
+          type="button"
+          className="NoteListNav__add-folder-button"
+        >
+          <FontAwesomeIcon icon="plus" />
+          <br />
+          Folder
+        </CircleButton>
+      </>
+    );
+  };
+
+  renderForm = () => {
+    // const { folders = [] } = this.context
     return (
       <section className='AddNote'>
         <h2>Create a note</h2>
@@ -52,29 +128,47 @@ export default class AddNote extends Component {
             <label htmlFor='note-name-input'>
               Name
             </label>
-            <input type='text' id='note-name-input' name='note-name' />
+            <input
+              type='text'
+              id='note-name-input'
+              name='note-name'
+              onChange={(e) => this.updateName(e.target.value)}
+            />
+            {this.state.name.touched && !this.validateName() ? (
+              <p>name is required</p>
+            ) : null}
           </div>
           <div className='field'>
             <label htmlFor='note-content-input'>
               Content
             </label>
-            <textarea id='note-content-input' name='note-content' />
+            <textarea
+              id='note-content-input'
+              name='note-content'
+              onChange={(e) => this.updateContent(e.target.value)}
+            />
           </div>
           <div className='field'>
             <label htmlFor='note-folder-select'>
               Folder
             </label>
-            <select id='note-folder-select' name='note-folder-id'>
-              <option value={null}>...</option>
-              {folders.map(folder =>
-                <option key={folder.id} value={folder.id}>
-                  {folder.name}
-                </option>
-              )}
+            <select
+              id='note-folder-select'
+              name='note-folder-id'
+              onChange={(e) => this.updateFolder(e.target.value)}
+            >
+              <option value={null}>Choose</option>
+              {this.renderFolderSelection()}
             </select>
+            {this.state.folder.touched && !this.validateFolder() ? (
+              <p>Folder is required</p>
+            ) : null}
           </div>
           <div className='buttons'>
-            <button type='submit'>
+            <button
+              disabled={!this.validateName() || !this.validateFolder()}
+              type='submit'
+            >
               Add note
             </button>
           </div>
@@ -82,4 +176,14 @@ export default class AddNote extends Component {
       </section>
     )
   }
+
+  render() {
+    return (
+      <>
+        {this.context.folders.length > 0 && this.renderForm()}
+        {this.context.folders.length <= 0 && this.renderAlt()}
+      </>
+    );
+  }
+
 }
